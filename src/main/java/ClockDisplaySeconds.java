23342b4598990b1bd23ce794bbd9a43744572a78
SrikarVem <69221759+ScodingV@users.noreply.github.com>
2024-09-21 00:17:34 -0700

    Update ClockDisplaySeconds.java

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,69 @@
+package src.main.java;
+
 
 public class ClockDisplaySeconds
 {
     //Extend ClockDisplaySeconds to include a seconds field.
     //Update all methods and parameters to accomodate this change
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+    
+    private String genDisplay;
+    //Same process as ClockDisplay, just adding on the seconds as well
+    public ClockDisplaySeconds(){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        updateDisplay();
+    }
+    
+    public ClockDisplaySeconds(int hour, int minute, int second){
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
+        
+        setTime(hour, minute, second);
+    }
+    public void timeTickSec() {
+        seconds.increment();
+        
+        if (seconds.getValue() == 0) {
+            minutes.increment();
+            if (minutes.getValue() == 0) {
+                hours.increment();
+            }
+        }
+        
+        updateDisplay();
+    }
+    
+    //Implement a method setTime that takes in 2 parameters, hour and minute
+    //The method should set the hours value and minutes value
+    //The updateDisplay method should be called before finishing
+    public void setTime(int hour, int minute, int second){
+        seconds.setValue(second);
+        minutes.setValue(minute);
+        hours.setValue(hour);
+        
+        updateDisplay();
+    }
+    //Implement a method getTime that takes no parameter and returns a String
+    //The return String should be formatted as HH:MM and report out the current time
+    public String getTime() {
+        return genDisplay;
+    }
+    //Implement a method updateDisplay that takes no parameters and returns nothing
+    //The method should update the displayString with the current time in a format
+    //  HH:MM
+    public void updateDisplay() {
+        String result = "";
+        result = hours.getDisplayValue();
+        result = result + ":";
+        result = result + minutes.getDisplayValue();
+        result = result + ":";
+        result = result + seconds.getDisplayValue();
+        
+        genDisplay = result;
+    }
 }
